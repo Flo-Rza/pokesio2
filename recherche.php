2bb437077618084ec845d1cd34e3f6ff20652c9d
Flo <florian.breq@gmail.com>
2017-03-17 11:55:38 +0100

    Correction de l'emplacement des annotation

--- recherche.php
+++ recherche.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    /* importation de bootstrap */
+    <?php /* importation de bootstrap */?>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport"
